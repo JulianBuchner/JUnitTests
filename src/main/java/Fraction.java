b25855b76dd9ee39df8e9c86d03356dcb487d8f6
JulianBuchner <jbuchner@htl-steyr.ac.at>
2022-10-05 08:34:20 +0200

    error

--- src/main/java/Fraction.java
+++ src/main/java/Fraction.java
@@ -23,6 +23,85 @@ public class Fraction {
         this.divisor = divisor;
     }
 
+    public Fraction add(Fraction other) {
+        /**
+         * @ToDo
+         * Add this with other
+         */
+        int thisNewDivisor = this.getDivisor() * other.getDivisor();
+        int thisNewDividend = this.getDividend() * other.getDivisor();
+
+        int otherNewDivisor = other.getDivisor() * this.getDivisor();
+        int otherNewDividend = other.getDividend() * this.getDivisor();
+
+        int dividend = thisNewDividend + otherNewDividend;
+        int divisor = thisNewDivisor + otherNewDivisor;
+
+        return new Fraction(dividend, divisor);
+    }
+
+    public Fraction subtract(Fraction other) {
+        /**
+         * @ToDo
+         * Subtract this with other
+         */
+        int thisNewDivisor = this.getDivisor() * other.getDivisor();
+        int thisNewDividend = this.getDividend() * other.getDivisor();
+
+        int otherNewDivisor = other.getDivisor() * this.getDivisor();
+        int otherNewDividend = other.getDividend() * this.getDivisor();
+
+        int dividend = thisNewDividend - otherNewDividend;
+        int divisor = thisNewDivisor - otherNewDivisor;
+
+        return new Fraction(dividend, divisor);
+    }
+
+    public Fraction divide(Fraction other) {
+        /**
+         * @ToDo
+         * Divide this by other
+         */
+        int dividend = this.getDividend() * other.getDivisor();
+        int divisor = this.getDivisor() * other.getDividend();
+
+        return new Fraction(dividend, divisor);
+    }
+
+    public Fraction multiply(Fraction other) {
+        /**
+         * @ToDo
+         * Multiply this with other
+         */
+        int dividend = this.getDividend() * other.getDividend();
+        int divisor = this.getDivisor() * other.getDivisor();
+
+        return new Fraction(dividend, divisor);
+    }
+
+    public Fraction shorten() {
+        /**
+         * @ToDo
+         * Shorten the fraction (kürzen)
+         */
+        int largerOne = Math.max(this.dividend, this.divisor);
+        int gcd = 0;
+
+        for (int i = largerOne; i >= 2; i--) {
+            if (this.dividend % i == 0 && this.divisor % i == 0) {
+                gcd = i;
+                break;
+            }
+        }
+
+        if (gcd != 0) {
+            this.dividend /= gcd;
+            this.divisor /= gcd;
+        }
+
+        return new Fraction(this.dividend, this.divisor);
+    }
+
     @Override
     public String toString() {
         return this.dividend + " / " + this.divisor;
--- src/test/java/FractionTest.java
+++ src/test/java/FractionTest.java
@@ -24,7 +24,7 @@ class FractionTest {
     void getDivisor() {
         Fraction f = new Fraction(1, 10);
 
-        Assertions.assertEquals(10, f.getDivisor());
+        Assertions.assertEquals(11, f.getDivisor());
     }
 
     @Test
@@ -40,5 +40,54 @@ class FractionTest {
         Fraction f = new Fraction(17, 3);
 
         Assertions.assertEquals("17 / 3", f.toString());
+    }
+
+    @Test
+    void add() {
+        Fraction f1 = new Fraction(10, 5);
+        Fraction f2 = new Fraction(15, 10);
+
+        Fraction f = f1.add(f2);
+
+        Assertions.assertEquals("175 / 100", f.toString());
+    }
+
+    @Test
+    void subtract() {
+        Fraction f1 = new Fraction(10, 5);
+        Fraction f2 = new Fraction(15, 10);
+
+        Fraction f = f1.subtract(f2);
+
+        Assertions.assertEquals("25 / 0", f.toString());
+    }
+
+    @Test
+    void divide() {
+        Fraction f1 = new Fraction(10, 5);
+        Fraction f2 = new Fraction(15, 10);
+
+        Fraction f = f1.divide(f2);
+
+        Assertions.assertEquals("100 / 75", f.toString());
+    }
+
+    @Test
+    void multiply() {
+        Fraction f1 = new Fraction(10, 5);
+        Fraction f2 = new Fraction(15, 10);
+
+        Fraction f = f1.multiply(f2);
+
+        Assertions.assertEquals("150 / 50", f.toString());
+    }
+
+    @Test
+    void shorten() {
+        Fraction f = new Fraction(20, 100);
+
+        f.shorten();
+
+        Assertions.assertEquals("1 / 5", f.toString());
     }
 }
